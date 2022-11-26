772a7afb4e4c06670aef9fb7fdd65473127520ae
Timo Laitila <timo.t.laitila@gmail.com>
2022-11-26 22:47:23 +0200

    Refactoring test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
 //
@@ -6,14 +8,14 @@
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -24,10 +26,16 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background ~somehow~
-fn spawn_app() {
-    let server = rust_api_poc::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    // Bind lister to OS default open port
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = rust_api_poc::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+    // Return the application address to the caller
+    format!("http://127.0.0.1:{}", port)
 }
